89263e19c412b1558a44573c6f9eb848ec697f65
OZKAN <alper.ozkan.p@gmail.com>
2019-03-26 08:49:23 +0100

    Deuxieme layout + intent

--- app/src/main/java/com/example/miniprojetandroid/MainActivity.java
+++ app/src/main/java/com/example/miniprojetandroid/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.miniprojetandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Les composants utilisés dans l'appli
+    Button envoyer;
+    EditText keywords; // L'input de texte tout en haut, TODO : Trouver un meilleur nom, comprendre ce qu'il fait
+    Spinner spinnerDate;
+    Spinner spinnerGenre;
+    SeekBar seekBarNombre;
+    // TODO : Ajouter des composants et fonctionalités, dans de nouvelles activités ?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // On instancie les compsants
+        envoyer = findViewById(R.id.envoyer);
+        keywords = findViewById(R.id.keywords);
+        spinnerDate = findViewById(R.id.spinnerDate);
+        spinnerGenre = findViewById(R.id.spinnerGenre);
+        seekBarNombre = findViewById(R.id.seekBarNombre);
+    // ...
+
+
+        // Les listeners
+        // Appui sur bouton envoyer, on envoie vers l'activité résultats
+        envoyer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent trouverFilms = new Intent(MainActivity.this, ResultActivity.class);
+                //trouverFilms.putExtra("keywords", keywords.getText());
+                startActivity(trouverFilms);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/miniprojetandroid/ResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.miniprojetandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    protected void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results);
+
+    }
+
+}
